f1268cf06e53d45dc8d05fa1518f4a5130035bbe
chenyuhao <cccchenyuhao@gmail.com>
2022-01-25 21:42:28 +0800

    update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,6 @@
 export const fetchLocationId = async (city) => {
   const response = await fetch(
-    `https://www.metaweather.com/api/location/search/?query=${city}`,
-    {
-      mode: "no-cors",
-    }
+    `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${city}`
   )
   const locations = await response.json()
   return locations[0].woeid
@@ -11,10 +8,7 @@ export const fetchLocationId = async (city) => {
 
 export const fetchWeather = async (woeid) => {
   const response = await fetch(
-    `https://www.metaweather.com/api/location/${woeid}/`,
-    {
-      mode: "no-cors",
-    }
+    `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`
   )
   const { title, consolidated_weather } = await response.json()
   const { weather_state_name, the_temp } = consolidated_weather[0]
